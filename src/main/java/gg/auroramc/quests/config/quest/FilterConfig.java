6daa007858601a94ce3a9d3d7bc10366be00ce0a
Erik Szabó <erik1szabo1@gmail.com>
2025-07-02 18:21:18 +0200

    Fix wrong key for min-ylevel and max-ylevel in default configs

--- src/main/java/gg/auroramc/quests/config/quest/FilterConfig.java
+++ src/main/java/gg/auroramc/quests/config/quest/FilterConfig.java
@@ -1,12 +1,14 @@
 package gg.auroramc.quests.config.quest;
 
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 @Getter
+@ToString
 public class FilterConfig {
     private SmartList worlds;
     private SmartList regions;
@@ -17,12 +19,14 @@ public class FilterConfig {
     private List<String> requirements = new ArrayList<>();
 
     @Getter
+    @ToString
     public static class SmartList {
         private String type;
         private Set<String> value;
     }
 
     @Getter
+    @ToString
     public static class HandConfig {
         private Set<String> items;
     }
